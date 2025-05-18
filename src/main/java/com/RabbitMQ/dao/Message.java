497f97931a7280d22983b21a3414386189f658fb
manishpandey <pandeym321@gmail.com>
2025-05-18 12:50:37 +0530

    fixed lombok issue

--- src/main/java/com/RabbitMQ/dao/Message.java
+++ src/main/java/com/RabbitMQ/dao/Message.java
@@ -2,48 +2,47 @@ package com.RabbitMQ.dao;
 
 import lombok.*;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Message {
     private String firstName;
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
 
     private String message;
-
-    public String getKeys() {
-        return keys;
-    }
-
-    public void setKeys(String keys) {
-        this.keys = keys;
-    }
+//
+//    public String getKeys() {
+//        return keys;
+//    }
+//
+//    public void setKeys(String keys) {
+//        this.keys = keys;
+//    }
 
     private String keys;
 
-    public String getFirstName() {
-        return firstName;
-    }
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" +
-                "firstName='" + firstName + '\'' +
-                ", message='" + message + '\'' +
-                ", keys='" + keys + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Message{" +
+//                "firstName='" + firstName + '\'' +
+//                ", message='" + message + '\'' +
+//                ", keys='" + keys + '\'' +
+//                '}';
+//    }
 }
 
